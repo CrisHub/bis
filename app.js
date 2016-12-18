3771ad86991f6bc796c28005e1858917f25f3b74
cristian moldovan <cristian moldovan>
2016-12-18 23:16:40 +0200

    ..

---
+++ app.js
@@ -0,0 +1,87 @@
+/*
+ * Shopify Embedded App. skeleton.
+ *
+ * Copyright 2014 Richard Bremner
+ * richard@codezuki.com
+ */
+
+var bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
+    express = require('express'),
+    routes = require('./routes'),
+    shopifyAuth = require('./routes/shopify_auth'),
+    path = require('path'),
+    nconf = require('nconf'),
+    db      = require('./models'),
+    cors = require('cors'),
+    morgan = require('morgan');
+
+//load settings from environment config
+// nconf.argv().env().file({
+//     file: 'dev-settings.json'
+// });
+nconf.set('oauth:api_key', process.env.api_key);
+nconf.set('oauth:client_secret', process.env.client_secret);
+nconf.set('oauth:redirect_url', "https://bookinstore.herokuapp.com/auth_token");
+nconf.set('oauth:scope', "read_products, write_products");
+
+exports.nconf = nconf;
+
+//configure express
+var app = express();
+
+//log all requests
+app.use(morgan('combined'));
+
+//support json and url encoded requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//setup encrypted session cookies
+app.use(cookieParser());
+app.use(cookieSession({
+    secret: "--express-session-encryption-key--"
+}));
+
+//statically serve from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//use jade templating engine for view rendering
+app.set('view engine', 'jade');
+
+//use the environment's port if specified
+app.set('port', process.env.PORT || 3000);
+
+var appAuth = new shopifyAuth.AppAuth();
+
+var whitelist = ['https://www.caramel.ro','http://www.caramel.ro'];
+
+var corsOptions = {
+  origin: function(origin, callback){
+      var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  },
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+//configure routes
+app.get('/', routes.index);
+app.get('/auth_app', appAuth.initAuth);
+app.get('/escape_iframe', appAuth.escapeIframe);
+app.get('/auth_code', appAuth.getCode);
+app.get('/auth_token', appAuth.getAccessToken);
+app.get('/render_app', routes.renderApp);
+app.get('/preorderd_products', routes.preorderProduct);
+app.get('/book-confirmation/:productId', routes.bookConfirmation);
+app.get('/view-product/:productId', routes.viewProduct);
+app.get('/delete-product/:variantId', routes.deleteProduct);
+app.get('/soft-delete-product/:variantId', routes.softDeleteProduct);
+app.post('/book-product', cors(corsOptions), routes.bookProduct);
+
+
+db.sequelize.sync().then(function() {
+  app.listen(app.get('port'), function() {
+      console.log('Listening on port ' + app.get('port'));
+  });
+});
---
+++ models/product.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define("Product", {
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    productId: DataTypes.STRING,
+    variantId: DataTypes.STRING,
+    quantity: DataTypes.STRING,
+    variant: DataTypes.STRING,
+    size: DataTypes.STRING,
+    customerFirstName: DataTypes.STRING,
+    customerLastName: DataTypes.STRING,
+    customerEmail: DataTypes.STRING,
+    customerPhone: DataTypes.STRING,
+    comments: DataTypes.STRING,
+    store: DataTypes.STRING,
+    type: DataTypes.STRING,
+    status: DataTypes.STRING,
+    link: DataTypes.STRING,
+    address: DataTypes.STRING,
+    deletedAt: DataTypes.STRING
+  });
+}
