b26002609c37f46bd51e65be81531a034397321e
cristian moldovan <cristian moldovan>
2016-12-23 23:30:06 +0200

    ..

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ nconf.set('oauth:api_key', process.env.api_key);
 nconf.set('oauth:client_secret', abc);
 nconf.set('oauth:redirect_url', "https://bookinstore.herokuapp.com/auth_token");
 nconf.set('oauth:scope', "read_products, write_products");
-console.log(abc);
+console.log(app.nconf.get('oauth:api_key'));
 exports.nconf = nconf;
 
 //configure express
