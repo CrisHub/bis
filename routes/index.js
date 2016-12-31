2bca2a8cb3b8b4a019f3581477fe6f6f5cbea9a7
cristian moldovan <cristian moldovan>
2016-12-31 19:50:34 +0200

    ..

--- routes/index.js
+++ routes/index.js
@@ -331,7 +331,7 @@ exports.viewProduct = function(req, res) {
 
 exports.getProducts = function(req, res) {
   var defaultFilters = {type:'book-in-store', deletedAt:null};
-  var filters = _.mearge(defaultFilters, req.query);
+  var filters = _.merge(defaultFilters, req.query);
   console.log(filters);
   db.Product.findAll({
       where:filters,
