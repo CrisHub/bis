67852e91cd84ab3b2997772106f4e7948bff6ede
cristian moldovan <cristian moldovan>
2017-01-08 17:02:56 +0200

    ..

--- client/app/scripts/controllers/preorderedProducts.js
+++ client/app/scripts/controllers/preorderedProducts.js
@@ -39,11 +39,13 @@ angular.module('sbAdminApp')
 
     $scope.unpicked = function(id) {
       $http.get('/delete-product/'+id).then(function(response) {
-        angular.forEach($scope.products, function(p, idx) {
-          if (p.id === response.data.id) {
-            $scope.products.splice(idx, 1);
-          }
-        });
+        if (response == 1) {
+          angular.forEach($scope.products, function(p, idx) {
+            if (p.id === id) {
+              $scope.products.splice(idx, 1);
+            }
+          });
+        }
         alertify.success('Product successfully deleted!');
       }, function(response) {
         alertify.error(response);
