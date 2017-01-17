a292376cdcd339ce6327cd467bcc6a7a240bb9c5
cristian moldovan <cristian moldovan>
2017-01-17 22:25:54 +0200

    ..

--- client/app/scripts/controllers/bookInStoreCtrl.js
+++ client/app/scripts/controllers/bookInStoreCtrl.js
@@ -20,12 +20,13 @@ angular.module('sbAdminApp')
   	alertify.logPosition("top left");
   	$scope.sendEmail = function(id) {
   		$http.get('/book-confirmation/'+id).then(function(response) {
-  			angular.forEach($scope.products, function(p, idx) {
-				if (p.id === response.data.id){
-					$scope.products[idx] = response.data;
-				}
-			});
-        if (status != 'error') {
+  			
+        if (response.status != 'error') {
+          angular.forEach($scope.products, function(p, idx) {
+            if (p.id === response.data.id){
+              $scope.products[idx] = response.data;
+            }
+          });
           alertify.success('Email successfully sent!');
         } else {
           alertify.error(response.message);
