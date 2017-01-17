570d88523d0ea6c401b689ad07935fea44bd87b6
cristian moldovan <cristian moldovan>
2017-01-17 23:19:01 +0200

    ..

--- client/app/scripts/controllers/bookInStoreCtrl.js
+++ client/app/scripts/controllers/bookInStoreCtrl.js
@@ -22,6 +22,7 @@ angular.module('sbAdminApp')
   		$http.get('/book-confirmation/'+id).then(function(response) {
   			
         if (response.status != 'error') {
+          console.log($scope.products.length);
           if ($scope.products.length == 1) {
             $state.go('dashboard.bookedProducts');
             return;
